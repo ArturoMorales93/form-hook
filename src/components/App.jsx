2702f8575e2b1e39e343000d90f5d8e704b5f94f
Arthur <jawallace.casta@gmail.com>
2021-09-04 14:17:12 -0600

    Test1 para hacer el build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import { HOME_PAGE, REQUERIMIENTOS_PAGE, FORMULARIO_PAGE, CODIGO_PAGE } from '..
 const App = () => (
   <Router>
     <Switch>
-      <Route path={HOME_PAGE.url} exact component={Home} />
+      {/* <Route path={HOME_PAGE.url} exact component={Home} /> */}
       <Route path={REQUERIMIENTOS_PAGE.url} component={Requerimientos} />
       <Route path={FORMULARIO_PAGE.url} component={Formulario} />
       <Route path={CODIGO_PAGE.url} component={Codigo} />
