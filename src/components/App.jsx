a3888658465cc8bb6019ff9ee6b8324c639cc43d
Arthur <jawallace.casta@gmail.com>
2021-09-04 13:36:22 -0600

    Se ajustan las rutas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import '../styles/styles.scss'
-import { HOME_PAGE, REQUERIMIENTOS_PAGE, FORMULARIO_PAGE, CODIGO_PAGE } from '../routes/routes'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Requerimientos from './Pages/Requerimientos'
-import RegistrationForm from './Pages/Formulario'
-import Codigo from './Pages/Codigo'
 import Home from './Pages/Home'
+import Codigo from './Pages/Codigo'
+import Formulario from './Pages/Formulario'
+import Requerimientos from './Pages/Requerimientos'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HOME_PAGE, REQUERIMIENTOS_PAGE, FORMULARIO_PAGE, CODIGO_PAGE } from '../routes/routes'
 
 const App = () => (
   <Router>
     <Switch>
       <Route path={HOME_PAGE.url} exact component={Home} />
       <Route path={REQUERIMIENTOS_PAGE.url} component={Requerimientos} />
-      <Route path={FORMULARIO_PAGE.url} component={RegistrationForm} />
+      <Route path={FORMULARIO_PAGE.url} component={Formulario} />
       <Route path={CODIGO_PAGE.url} component={Codigo} />
     </Switch>
   </Router>
