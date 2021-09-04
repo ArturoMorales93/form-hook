d57ce7802b1ca96a0ff43012f54da77e38bf111f
Arthur <jawallace.casta@gmail.com>
2021-09-04 14:29:28 -0600

    Se vuelve a iniciar de cero el archivo App.jsx ya que está teniendo problemas con el build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,14 @@
-import '../styles/styles.scss'
-// import Home from './Pages/Home'
-import Codigo from './Pages/Codigo'
-import Formulario from './Pages/Formulario'
-import Requerimientos from './Pages/Requerimientos'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { REQUERIMIENTOS_PAGE, FORMULARIO_PAGE, CODIGO_PAGE } from '../routes/routes'
+import "../styles/styles.scss"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HOME_PAGE } from "../routes/routes";
+import Home from "./Pages/Home";
 
 const App = () => (
   <Router>
     <Switch>
-      {/* <Route path={HOME_PAGE.url} exact component={Home} /> */}
-      <Route path={REQUERIMIENTOS_PAGE.url} component={Requerimientos} />
-      <Route path={FORMULARIO_PAGE.url} component={Formulario} />
-      <Route path={CODIGO_PAGE.url} component={Codigo} />
+      <Route path={HOME_PAGE.url} exact component={Home} />
     </Switch>
   </Router>
 )
 
-export default App;
+export default App
